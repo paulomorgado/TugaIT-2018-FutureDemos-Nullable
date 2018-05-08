b0554bcf99e71656410efc80ae80a1040d9be85d
Dustin Campbell <dustin@teamcampbell.org>
2018-05-08 14:57:58 -0700

    Initial commit

---
+++ src/Nullable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var miguel = new Person("Miguel", "de Icaza");
+        int length = GetLengthOfMiddleName(miguel);
+        WriteLine(length);
+    }
+
+    static int GetLengthOfMiddleName(Person p)
+    {
+        return p.MiddleName.Length;
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public Person(string firstName, string middleName, string lastName)
+    {
+        FirstName = firstName;
+        MiddleName = middleName;
+        LastName = lastName;
+    }
+}
