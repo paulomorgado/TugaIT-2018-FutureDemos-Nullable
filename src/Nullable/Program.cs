62613751013827be46db12808288c6217892ff09
Paulo Morgado <paulo@paulomorgado.net>
2018-07-19 22:08:22 +0100

    Nullable string

--- src/Nullable/Program.cs
+++ src/Nullable/Program.cs
@@ -19,7 +19,7 @@ class Program
 public class Person
 {
     public string FirstName { get; set; }
-    public string MiddleName { get; set; }
+    public string? MiddleName { get; set; }
     public string LastName { get; set; }
 
     public Person(string firstName, string lastName)
